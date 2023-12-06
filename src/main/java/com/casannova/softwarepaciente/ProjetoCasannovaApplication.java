05a4a1c0d868b3a9425adcc68af5ab482c27069d
Edgar Junior <edgarlsj@gmail.com>
2023-12-06 10:17:14 -0400

    criação inicial do projeto

---
+++ src/main/java/com/casannova/softwarepaciente/ProjetoCasannovaApplication.java
@@ -0,0 +1,13 @@
+package com.casannova.softwarepaciente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCasannovaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoCasannovaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/casannova/softwarepaciente/ProjetoCasannovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casannova.softwarepaciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCasannovaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
